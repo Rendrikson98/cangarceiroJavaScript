4b07b38b986a205ac05de3036171e9bfd8cfb259
Rendrikson Oliveira <40209027+Rendrikson98@users.noreply.github.com>
2021-02-19 17:10:45 -0300

    finished the chapter 2

--- capitulo 02/client/app/domain/negociacao/Negociacao.js
+++ capitulo 02/client/app/domain/negociacao/Negociacao.js
@@ -1,10 +1,9 @@
 class Negociacao {
 
-    constructor(data, quantidade, valor) {
-        this._data = new Date(data.getTime());
-        this._quantidade = quantidade;
-        this._valor = valor;
-        Object.freeze(this)
+    constructor(_data, _quantidade, _valor) {
+        Object.assign(this, { _quantidade, _valor });
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
     }
 
     getVolume() {
